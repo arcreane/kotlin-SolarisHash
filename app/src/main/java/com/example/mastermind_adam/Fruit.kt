a98ffef610578157ead21a5373ed0f7347e34294
SolarisHash <77726918+SolarisHash@users.noreply.github.com>
2024-02-06 17:20:03 +0100

    Création class Fruit

---
+++ app/src/main/java/com/example/mastermind_adam/Fruit.kt
@@ -0,0 +1,7 @@
+package com.example.mastermind_adam
+
+class Fruit(val name: String, val hasSeeds: Boolean, val isPeelable: Boolean) {
+    override fun toString(): String {
+        return name
+    }
+}
